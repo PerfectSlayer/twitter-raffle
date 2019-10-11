406432ac5fb79c2471cb7292fc1c7951401bbe52
PerfectSlayer <bruce.bujon@gmail.com>
2019-10-11 19:26:41 +0200

    chore: Remove test

--- src/test/java/fr/hardcoding/NativeExampleResourceIT.java
+++
@@ -1,9 +0,0 @@
-package fr.hardcoding;
-
-import io.quarkus.test.junit.SubstrateTest;
-
-@SubstrateTest
-public class NativeExampleResourceIT extends ExampleResourceTest {
-
-    // Execute the same tests but in native mode.
-}
