65345543b90da7dd8170e16ad74cde4627bd83dd
PerfectSlayer <bruce.bujon@gmail.com>
2019-10-19 19:21:08 +0200

    feat: Improve raffle
    Limit query to twitter to first 100 results
    Return SERVICE_UNAVAILABLE on error
    Log Twitter rate limit status on query

--- src/main/java/fr/hardcoding/TwitterRaffle.java
+++ src/main/java/fr/hardcoding/TwitterRaffle.java
@@ -1,72 +1,73 @@
 package fr.hardcoding;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.Set;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import java.util.stream.Collectors;
-import java.util.List;
-import java.util.Map;
-import java.util.Random;
-
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
-import javax.ws.rs.core.MediaType;
 
 import twitter4j.Query;
 import twitter4j.QueryResult;
+import twitter4j.RateLimitStatus;
 import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Response;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+import static javax.ws.rs.core.Response.Status.SERVICE_UNAVAILABLE;
+
 @Path("/")
 public class TwitterRaffle {
     private static final Logger LOGGER = Logger.getLogger(TwitterRaffle.class.getName());
-    private static final int WINER_COUNT = 10;
+    private static final int WINNER_COUNT = 10;
+    private static final int MAX_RESULT = 100;
     private Twitter twitter = new TwitterFactory().getInstance();
 
     @Path("/raffle")
     @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public List<Winner> hello(@QueryParam("speaker") String speaker) {
-        return performRaffle(speaker);
+    @Produces(APPLICATION_JSON)
+    public Response hello(@QueryParam("speaker") String speaker) {
+        try {
+            List<Winner> winners = performRaffle(speaker);
+            return Response.ok(winners).build();
+        } catch (TwitterException exception) {
+            LOGGER.log(Level.SEVERE, "Failed to query tweets", exception);
+            return Response.status(SERVICE_UNAVAILABLE).build();
+        }
     }
 
-    private List<Winner> performRaffle(String screenName) {
+    private List<Winner> performRaffle(String screenName) throws TwitterException {
         Map<String, List<Status>> userTweets;
-        try {
-            Query query = getQuery(screenName);
-            userTweets = performQuery(query);
-        } catch (TwitterException exception) {
-            LOGGER.log(Level.SEVERE, "Failed to query twitter.", exception);
-            return Collections.emptyList();
-        }
+        Query query = getQuery(screenName);
+        userTweets = performQuery(query);
 
         Random rand = new Random(System.currentTimeMillis());
         List<String> users = new LinkedList<>(userTweets.keySet());
         Set<String> winningUsers = new HashSet<>();
 
-        while (winningUsers.size() < Math.min(WINER_COUNT, userTweets.size())) {
+        while (winningUsers.size() < Math.min(WINNER_COUNT, userTweets.size())) {
             String winner = users.remove(rand.nextInt(users.size()));
             winningUsers.add(winner);
         }
 
-        List<Winner> winners = winningUsers.stream()
+        return winningUsers.stream()
                 .map(userTweets::get)
                 .map(list -> list.get(0))
                 .map(Winner::fromStatus)
                 .collect(Collectors.toList());
-
-        LOGGER.fine(winners.get(0).toString());
-
-        return winners;
     }
 
     private Query getQuery(String screenName) {
@@ -83,16 +84,20 @@ public class TwitterRaffle {
         Map<String, List<Status>> userTweets = new HashMap<>();
         QueryResult result;
         do {
-            result = twitter.search(query);
-            // RateLimitStatus limit = result.getRateLimitStatus();
-            // log.info(String.format("Rate Limit for Query: %s/%s. Reset in %s seconds",
-            // limit.getRemaining(), limit.getLimit(), limit.getSecondsUntilReset()));
+            result = this.twitter.search(query);
+            RateLimitStatus limit = result.getRateLimitStatus();
+            LOGGER.info(String.format(
+                    "Rate Limit for Query: %s/%s. Reset in %s seconds",
+                    limit.getRemaining(),
+                    limit.getLimit(),
+                    limit.getSecondsUntilReset()
+            ));
             List<Status> tweets = result.getTweets();
             for (Status tweet : tweets) {
                 String screenName = tweet.getUser().getScreenName();
                 userTweets.computeIfAbsent(screenName, __ -> new ArrayList<>()).add(tweet);
             }
-        } while ((query = result.nextQuery()) != null);
+        } while ((query = result.nextQuery()) != null && userTweets.size() < MAX_RESULT);
         return userTweets;
     }
 }
